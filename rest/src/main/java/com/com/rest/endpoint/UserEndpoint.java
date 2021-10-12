926d9835fd5fde3d0738491b711a8426309162a7
Vahann <yan.vahan@gmail.com>
2021-10-12 17:09:19 +0400

    endpoint test

--- rest/src/main/java/com/com/rest/endpoint/UserEndpoint.java
+++ rest/src/main/java/com/com/rest/endpoint/UserEndpoint.java
@@ -3,22 +3,50 @@ package com.com.rest.endpoint;
 import com.com.common.model.User;
 import com.com.common.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserEndpoint {
 
-//
+    //
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
-    public List<User> getAllUser(){
+    @GetMapping("/")
+    public List<User> getAllUser() {
+        //service
         return userRepository.findAll();
-
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") int id) {
+        //service
+        Optional<User> userById = userRepository.findById(id);
+        if (userById.isEmpty()) {
+            return ResponseEntity
+                    .notFound()
+                    .build();
+        }
+
+        return ResponseEntity.ok(userById.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUserById(@PathVariable("id") int id) {
+        //service
+       Optional<User> userById=userRepository.findById(id);
+        if (userById.isEmpty()) {
+            return ResponseEntity
+                    .notFound()
+                    .build();
+        }
+
+        userRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
